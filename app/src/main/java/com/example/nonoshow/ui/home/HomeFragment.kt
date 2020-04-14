6fbb187e7505b53aca55eb261d21d744f5f512cf
박해범 <beom0528@naver.com>
2020-04-14 23:42:43 +0900

    Home fragment - 핸드폰번호로 검색
    editText - textView 간의 실시간 데이터 동기화 + 입력글자수제한 및 11글자 이상입력시 핸드폰번호로 검색하는 함수선언(검색함수는 미구현) -> 추후, search 함수로 데이터 불러온뒤에 textView 조작으로 변경해야함

--- app/src/main/java/com/example/nonoshow/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/nonoshow/ui/home/HomeFragment.kt
@@ -1,19 +1,47 @@
 package com.example.nonoshow.ui.home
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
 import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.example.nonoshow.R
+import kotlinx.android.synthetic.main.fragment_home.*
 
 class HomeFragment : Fragment() {
 
     private lateinit var homeViewModel: HomeViewModel
+    private lateinit var textView: TextView
+    private lateinit var edittext: EditText
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?){
+        super.onViewCreated(view, savedInstanceState)
+
+        val textView : TextView = getView()!!.findViewById(R.id.textView)
+        val edittext : EditText = getView()!!.findViewById(R.id.Text_search)
+
+        edittext.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                var phoneNum = s.toString()
+                textView.setText(phoneNum)
+
+                if(phoneNum.length > 10){
+                    search(phoneNum)
+                }
+            }
+        })
+    }
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -23,5 +51,9 @@ class HomeFragment : Fragment() {
             ViewModelProviders.of(this).get(HomeViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_home, container, false)
         return root
+    }
+
+    fun search(num : String){
+        // 여기에 검색함수
     }
 }
