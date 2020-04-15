5b70cda328db94233fc467a944fada9824d9dec6
박해범 <beom0528@naver.com>
2020-04-15 13:52:30 +0900

    Home fragment - 홈화면 확률컨트롤 레이아웃 기초 작성
    현재는 백엔드없이 랜덤으로 확률을 가져온다. 핸드폰번호로 검색부분에서 text 의 입력이 전부 완료된 후 작동되도록 바꾸고 싶음.

--- app/src/main/java/com/example/nonoshow/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/nonoshow/ui/home/HomeFragment.kt
@@ -6,25 +6,33 @@ import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.EditText
+import android.widget.ImageButton
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
 import com.example.nonoshow.R
+import de.hdodenhof.circleimageview.CircleImageView
 import kotlinx.android.synthetic.main.fragment_home.*
+import kotlin.random.Random
 
 class HomeFragment : Fragment() {
 
     private lateinit var homeViewModel: HomeViewModel
     private lateinit var textView: TextView
     private lateinit var edittext: EditText
+    private lateinit var circleImageButton : ImageButton
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?){
         super.onViewCreated(view, savedInstanceState)
 
         val textView : TextView = getView()!!.findViewById(R.id.textView)
         val edittext : EditText = getView()!!.findViewById(R.id.Text_search)
+        val circleImageButton : Button = getView()!!.findViewById(R.id.circleImageButton)
 
+        circleImageButton.textSize = 25f
+        circleImageButton.text = "X%"
         edittext.addTextChangedListener(object : TextWatcher {
             override fun afterTextChanged(s: Editable?) {
             }
@@ -34,10 +42,10 @@ class HomeFragment : Fragment() {
 
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
                 var phoneNum = s.toString()
-                textView.setText(phoneNum)
+                textView.text = phoneNum
 
                 if(phoneNum.length > 10){
-                    search(phoneNum)
+                    circleImageButton.text = search(phoneNum)
                 }
             }
         })
@@ -53,7 +61,13 @@ class HomeFragment : Fragment() {
         return root
     }
 
-    fun search(num : String){
+    fun search(num : String) : String{
         // 여기에 검색함수
+        val randomValue = Random.nextInt(0,100)
+        return setTextInCircle(randomValue)
+    }
+
+    private fun setTextInCircle(x : Int): String{
+        return x.toString() + "%"
     }
 }
